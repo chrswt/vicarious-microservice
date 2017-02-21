165fbb4cc8e30c4912ab90ecf83c106b39abd54d
Christopher Tham <chriswt91@gmail.com>
2017-02-20 19:05:44 -0800

    Add first test case based on sample input

--- tests/translation.py
+++ tests/translation.py
@@ -4,7 +4,16 @@ import requests
 
 class TranslationTests(unittest.TestCase):
     def setUp(self):
-        pass
+        self.url = 'http://127.0.0.1/api/translate'
+
+    def test_given_words(self):
+        test_words = ['pig', 'banana', 'trash', 'happy', 'duck', 'glove',
+                      'eat', 'omelet', 'are']
+        expected_words = ['igpay', 'ananabay', 'ashtray', 'appyhay', 'uckday',
+                          'oveglay', 'eatyay', 'omeletyay', 'areyay']
+
+        responses = [requests.post(self.url, x).text for x in test_words]
+        self.assertEqual(responses, expected_words, 'Responses should match')
 
 
 if __name__ == '__main__':
