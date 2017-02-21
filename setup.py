3e99025d9380f341d5319e315027936660eb6ec4
Christopher Tham <chriswt91@gmail.com>
2017-02-20 18:55:37 -0800

    Pipe requirements.txt into setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import find_packages, setup
 
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='pig_latin_microservice',
       version='1.0',
       description='Vicarious Pig Latin Microservice Project',
       author='Christopher Tham',
       platforms=['any'],
       license='MIT',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=required)
