cfd0d0268e666598b7c0613c0c408c41142f981b
Christopher Tham <chriswt91@gmail.com>
2017-02-20 19:09:31 -0800

    Implement translation route with request echo

--- services/translation.py
+++ services/translation.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, make_response, request
 
 
 app = Flask(__name__)
 
+@app.route('/api/translate', methods=['POST'])
+def on_post():
+    """Handles API route for English -> Pig Latin translation"""
+    data = request.stream.read().decode('utf-8')
+
+    if not data:
+        response = make_response('Error: POST requests for translations '
+                                 'must contain at least one word.')
+        response.status_code = 406 # Not Acceptable
+        return response
+
+    elif type(data) is not str:
+        response = make_response('Error: POST requests for translations must'
+                                 'be provided in the "text/plain" format.')
+        response.status_code = 415 # Unsupported Media Type
+        return response
+
+    response = make_response(data)
+    response.status_code = 200 # OK
+    response.content_type = 'text/plain'
+    return response
 
 if __name__ == '__main__':
     app.run(port=80, debug=True)
