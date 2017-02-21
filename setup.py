247c57b08f9f7baf8d0fb71ca323ac60005f280f
Christopher Tham <chriswt91@gmail.com>
2017-02-20 18:35:40 -0800

    Initialize setup file for microservice

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(name='pig_latin_microservice',
+      version='1.0',
+      description='Vicarious Pig Latin Microservice Project',
+      author='Christopher Tham',
+      platforms=['any'],
+      license='MIT',
+      packages=find_packages())
