24aee749ea7c917134173116a1748c4e3ce3ca46
Christopher Tham <chriswt91@gmail.com>
2017-02-20 19:16:44 -0800

    Implement single word translation

--- services/translation.py
+++ services/translation.py
@@ -3,6 +3,23 @@ from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
+def translate_word(word):
+    """Translates a single word to Pig Latin"""
+    vowels = 'aeiou'
+
+    # Case: word begins with a vowel
+    if word[0] in vowels:
+        return word + 'yay'
+
+    # Case: word begins with a consonant        
+    else:
+        for index, char in enumerate(word):
+            if char in vowels:
+                return word[index:] + word[:index] + 'ay'
+
+    # Case: word contains no vowels
+    return word + 'ay'
+
 @app.route('/api/translate', methods=['POST'])
 def on_post():
     """Handles API route for English -> Pig Latin translation"""
@@ -20,7 +37,7 @@ def on_post():
         response.status_code = 415 # Unsupported Media Type
         return response
 
-    response = make_response(data)
+    response = make_response(translate_word(data))
     response.status_code = 200 # OK
     response.content_type = 'text/plain'
     return response
